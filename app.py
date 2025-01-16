e1184a08e3b935663856cb504a2f13409a0611cd
ErikAhlinSWE <erik.ahlin@live.se>
2025-01-16 15:39:11 +0100

    db creaded in mysql container

--- app.py
+++ app.py
@@ -95,8 +95,8 @@ def contactpage():
 
 #if __name__  == "__main__":
 with app.app_context():
-#    db.create_all()
+    db.create_all()
 #    upgrade()
 
-#    seedData(db)
+    seedData(db)
     app.run()
